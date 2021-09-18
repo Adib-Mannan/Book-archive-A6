01ac41319aeb4c60a145ee671a8555bd77659f5c
AquiLiusOP <adibmannanrafi2003@gmail.com>
2021-09-18 21:26:09 +0600

    2ndcommit

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+// spinner 
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
 const searchBook = () => {
     const searchFeild = document.getElementById('search-feild');
+    toggleSpinner('block');
     const searchText = searchFeild.value;
     searchFeild.value = '';
+    // error handle 
     if (searchText === '') {
         const error = document.getElementById('error-2');
-        error.innerHTML = 'Please Write something!'
+        error.innerHTML = 'Please Write something!';
+        toggleSpinner('none')
     }
     else {
+        const error = document.getElementById('error-2');
+        error.innerHTML = '';
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
@@ -20,27 +30,36 @@ const searchBook = () => {
 const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+    // error handle 
     if (books.length === 0) {
         const error = document.getElementById('error-1');
         error.innerHTML = 'No Result found!! Give a valid book name '
+        toggleSpinner('none');
     }
-    books.forEach(book => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Book Name : ${book.title}</h5>
-                    <p class="card-text">Author Name : ${book.author_name}</p>
-                    <p class="card-text">Publisher : ${book.publisher}</p>
-                    <p class="card-text">first publish year : ${book.first_publish_year}</p>
+    else {
+        const error = document.getElementById('error-1');
+        error.innerHTML = '';
 
-                </div>
-            </div>`
-        searchResult.appendChild(div);
-    });
+        books.forEach(book => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+          <div class="card h-100">
+              <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                  <h5 class="card-title">Book Name : ${book.title}</h5>
+                  <p class="card-text">Author Name : ${book.author_name}</p>
+                  <p class="card-text">Publisher : ${book.publisher}</p>
+                  <p class="card-text">first publish year : ${book.first_publish_year}</p>
+
+              </div>
+          </div>`
+            searchResult.appendChild(div);
+        });
+        toggleSpinner('none');
+    }
 }
+// search amount handle 
 const displaySearchAmount = amount => {
     const h1 = document.getElementById('h1');
     h1.textContent = '';
